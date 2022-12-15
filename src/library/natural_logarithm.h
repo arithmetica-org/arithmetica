1ce8a6724327fcd131793a5605df2dfc1ccaf24e
Avighna <avighnakc@gmail.com>
2022-12-15 13:06:04 +0530

    Made natural_logarithm faster by utilizing a power function instead of manually computing 2^accuracy.

--- src/library/natural_logarithm.h
+++ src/library/natural_logarithm.h
@@ -2,35 +2,30 @@
 #define _natural_logarithm_h_
 
 #include "arithmetica.h"
+#include "power_integer.h"
 #include <basic_math_operations.h>
 #include <stdlib.h>
 #include <string.h>
 
 char *natural_logarithm(const char *n, size_t accuracy) {
   size_t _accuracy = accuracy;
-  accuracy = (size_t)((double)accuracy *
-                      4); // 10/3 is the real constant, but 4 further ensures
-                          // that the answer is accurate.
+  char unity[] = "1";
+  char two[] = "2";
+  accuracy *= 4; // 10/3 is the real constant, but 4 further ensures
+                 // that the answer is accurate.
   char *sqrt = (char *)calloc(strlen(n) + 1, 1);
   strcpy(sqrt, n);
-  char *two_power_n = (char *)calloc(2, 1);
-  two_power_n[0] = '1';
-  char unity[] = "1";
+  char *two_power_acc = power_integer(two, (long)accuracy, 0);
   for (size_t i = 0; i < accuracy; i++) {
-    char *buf = (char *)calloc(strlen(two_power_n) + 2, 1);
-    add(two_power_n, two_power_n, buf);
-    two_power_n = (char *)realloc(two_power_n, strlen(buf) + 1);
-    strcpy(two_power_n, buf);
     char *_buf = square_root(sqrt, accuracy);
     sqrt = (char *)realloc(sqrt, strlen(_buf) + 1);
     strcpy(sqrt, _buf);
     free(_buf);
-    free(buf);
   }
   char *buf = (char *)calloc(strlen(sqrt) + 2, 1);
   subtract(sqrt, unity, buf);
-  char *answer = (char *)calloc(strlen(buf) + strlen(two_power_n) + 3, 1);
-  multiply(buf, two_power_n, answer);
+  char *answer = (char *)calloc(strlen(buf) + strlen(two_power_acc) + 3, 1);
+  multiply(buf, two_power_acc, answer);
   size_t dec_loc = 0;
   for (size_t i = 0; i < strlen(answer); i++) {
     if (answer[i] == '.') {
@@ -42,7 +37,7 @@ char *natural_logarithm(const char *n, size_t accuracy) {
     answer[dec_loc + _accuracy + 1] = 0;
   free(buf);
   free(sqrt);
-  free(two_power_n);
+  free(two_power_acc);
   return answer;
 }
 
