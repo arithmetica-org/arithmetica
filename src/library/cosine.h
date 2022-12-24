ff1714dfd57a03ff6fc83b5749c73038f82b45de
Avighna <avighnakc@gmail.com>
2022-12-24 16:56:11 +0530

    Fixed accuracy bug in cosine.

--- src/library/cosine.h
+++ src/library/cosine.h
@@ -12,20 +12,21 @@ char *cosine(const char *number, size_t accuracy) {
   // cos(x) = sin(pi / 2 - x)
   // = sin(arcsin(1) - x)
 
+  size_t num_accuracy = number + strlen(number) - 1 - strchr(number, '.');
+  num_accuracy += 2; // to keep final answer accuracy high
   char _half_pi[] = "1."
                     "5707963267948966192313216916397514420985846996875529104874"
                     "722961539082031431044993140174126710585339";
-  char *half_pi = (char *)calloc(accuracy + 3, 1);
-  if (accuracy > 100) {
+  char *half_pi = (char *)calloc(num_accuracy + 3, 1);
+  if (num_accuracy > 100) {
     free(half_pi);
-    half_pi = arcsin("1", accuracy);
+    half_pi = arcsin("1", num_accuracy);
   } else {
-    truncate(_half_pi, accuracy);
+    truncate(_half_pi, num_accuracy);
     strcpy(half_pi, _half_pi);
   }
   char *sine_arg = (char *)calloc(strlen(half_pi) + strlen(number) + 3, 1);
   subtract(half_pi, number, sine_arg);
-  truncate(sine_arg, number + strlen(number) - 1 - strchr(number, '.'));
   char *answer = sine(sine_arg, accuracy);
 
   free(half_pi);
