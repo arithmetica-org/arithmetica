e7493ef64f4ddb8f41dd8ae6972776b942316393
Avighna <avighnakc@gmail.com>
2023-01-30 21:40:24 +0530

    Added arithmetica.hpp.

--- src/library/arithmetica.h
+++ src/library/arithmetica.h
@@ -12,43 +12,43 @@
 /// @brief Computes the inverse cosine of a number less than 1.
 /// @param number The number to compute the inverse cosine of.
 /// @param accuracy The number of decimal places to calculate accurately to.
-/// @return The inverse cosine of the first argument. Free this pointer one it's
-/// done being used.
-arithmetica_function_start char *arccos(const char *number, size_t accuracy);
+/// @return The inverse cosine of the first argument. Free this pointer one
+/// it's done being used.
+arithmetica_function_start char *arccos (const char *number, size_t accuracy);
 
 /// @brief Computes the inverse sine of a number less than 1.
 /// @param number The number to compute the inverse sine of.
 /// @param accuracy The number of decimal places to calculate accurately to.
 /// @return The inverse sine of the first argument. Free this pointer one it's
 /// done being used.
-arithmetica_function_start char *arcsin(const char *number, size_t accuracy);
+arithmetica_function_start char *arcsin (const char *number, size_t accuracy);
 
 /// @brief Computes the inverse tangent of a number less than 1.
 /// @param x The number to compute the inverse tangent of.
 /// @param accuracy The number of decimal places to calculate accurately to.
 /// @return The inverse tangent of the first argument. Free this pointer once
 /// it's done being used.
-arithmetica_function_start char *arctan(const char *number, size_t accuracy);
+arithmetica_function_start char *arctan (const char *number, size_t accuracy);
 
 /// @brief Computes the cosine of the first argument.
 /// @param number The angle to compute the cosine of, in radians.
 /// @param accuracy The number of decimal places to calculate accuractely to.
 /// @return The cosine of the first argument. Free this pointer once it's done
 /// being used.
-arithmetica_function_start char *cosine(const char *number, size_t accuracy);
+arithmetica_function_start char *cosine (const char *number, size_t accuracy);
 
 /// @brief Computes e^x, where e is Euler's number: the base to the natural
 /// logarithm.
 /// @param x The number to raise e to.
 /// @param accuracy The number of decimal places to calculate accurately to.
 /// @return The value of e^x. Free this pointer once it's done being used.
-arithmetica_function_start char *exponential(const char *x, size_t accuracy);
+arithmetica_function_start char *exponential (const char *x, size_t accuracy);
 
 /// @brief Finds the factorial of the first argument and stores the result in
 /// the second argument.
 /// @param n The number to find the factorial of.
 /// @param factorial Where the factorial of [n] will be stored.
-arithmetica_function_start void factorial(unsigned long n, char *factorial);
+arithmetica_function_start void factorial (unsigned long n, char *factorial);
 
 /// @brief Converts a non-negative rational fraction to a continued fraction.
 /// @param numerator_in The numerator of the fraction.
@@ -56,22 +56,22 @@ arithmetica_function_start void factorial(unsigned long n, char *factorial);
 /// @return A pointer to a char * containing the continued fraction
 /// representation of the input fraction. Safe to print; free once done using.
 arithmetica_function_start char *
-fraction_to_continued_fraction(const char *numerator_in,
-                               const char *denominator_in);
+fraction_to_continued_fraction (const char *numerator_in,
+                                const char *denominator_in);
 
 /// @brief Finds the greatest common divisor of the first and second integral
 /// arguments.
 /// @param a The first non-negative number to find the gcd of.
 /// @param b The second non-negative number to find the gcd of.
 /// @param gcd The gcd of the first two arguments.
-arithmetica_function_start void igcd(const char *a, const char *b, char *gcd);
+arithmetica_function_start void igcd (const char *a, const char *b, char *gcd);
 
 /// @brief Finds the lowest common multiple of the first and second integral
 /// arguments.
 /// @param a The first non-negative number to find the lcm of.
 /// @param b The second non-negative number to find the lcm of.
 /// @param lcm The lcm of the first two arguments.
-arithmetica_function_start void ilcm(const char *a, const char *b, char *lcm);
+arithmetica_function_start void ilcm (const char *a, const char *b, char *lcm);
 
 /// @brief Computes the natural logarithm of a non-negative number
 /// to any decimal place.
@@ -79,29 +79,29 @@ arithmetica_function_start void ilcm(const char *a, const char *b, char *lcm);
 /// @param accuracy The number of decimal places to calculate accurately to.
 /// @return A pointer to a char * containing the natural logarithm of the
 /// input number.
-arithmetica_function_start char *natural_logarithm(const char *n,
-                                                   size_t accuracy);
+arithmetica_function_start char *natural_logarithm (const char *n,
+                                                    size_t accuracy);
 
 /// @brief Computes x_in^n_in where x_in and n_in are two real numbers.
 /// @param x_in The real number base.
 /// @param n_in The real number exponent.
 /// @param accuracy The number of decimal places to calculate accurately to.
 /// @return A pointer to a char * containing x_in^n_in.
-arithmetica_function_start char *power(const char *x_in, const char *n_in,
-                                       size_t accuracy);
+arithmetica_function_start char *power (const char *x_in, const char *n_in,
+                                        size_t accuracy);
 
 /// @brief Converts a repeating decimal to a fraction
 /// @param non_repeating_part The part of the fraction (including the part
 /// before the decimal place) that does not repeat. For example, in
 /// 0.3343434..., "0.3" would be the non-repeating part.
-/// @param repeating_part The part of the fraction which repeats. For example in
-/// 0.3343434..., "34" would be the repeating part.
+/// @param repeating_part The part of the fraction which repeats. For example
+/// in 0.3343434..., "34" would be the repeating part.
 /// @param numerator_out The output numerator of the fraction.
 /// @param denominator_out The output denominator of the fraction.
 arithmetica_function_start void
-repeating_decimal_to_fraction(const char *non_repeating_part,
-                              const char *repeating_part, char *numerator_out,
-                              char *denominator_out);
+repeating_decimal_to_fraction (const char *non_repeating_part,
+                               const char *repeating_part, char *numerator_out,
+                               char *denominator_out);
 
 /// @brief Simplies an arithmetic expression involving the five basic math
 /// operations: addition, subtraction, multiplication, division, and
@@ -112,35 +112,35 @@ repeating_decimal_to_fraction(const char *non_repeating_part,
 /// output a decimal, one to output a fraction, and two to output a mixed
 /// fraction. Output types one and two will calculate everything with maximum
 /// precision.
-/// @param accuracy The accuracy of the divisions and exponentiations performed.
-/// You only need to worry about this if you have fractional exponents in your
-/// input, or if you have divisions in output type zero.
+/// @param accuracy The accuracy of the divisions and exponentiations
+/// performed. You only need to worry about this if you have fractional
+/// exponents in your input, or if you have divisions in output type zero.
 /// @return The simplified expression.
 arithmetica_function_start char *
-simplify_arithmetic_expression(const char *expression_in, int outputType,
-                               size_t accuracy);
+simplify_arithmetic_expression (const char *expression_in, int outputType,
+                                size_t accuracy);
 
 /// @brief Computes the sine of the first argument.
 /// @param number The angle to compute the sine of, in radians.
 /// @param accuracy The number of decimal places to calculate accuractely to.
 /// @return The sine of the first argument. Free this pointer once it's done
 /// being used.
-arithmetica_function_start char *sine(const char *number, size_t accuracy);
+arithmetica_function_start char *sine (const char *number, size_t accuracy);
 
 /// @brief Computes the square root of the first argument.
 /// @param number The non-negative number to compute the square root of.
 /// @param accuracy The number of decimal places to calculate accurately to.
 /// @return The square root of the first argument. Free this pointer once it's
 /// done being used.
-arithmetica_function_start char *square_root(const char *number_in,
-                                             size_t accuracy);
+arithmetica_function_start char *square_root (const char *number_in,
+                                              size_t accuracy);
 
 /// @brief Computes the trignometric tangent of the first argument.
 /// @param number The angle to compute the tangent of, in radians.
 /// @param accuracy The number of decimal places to calculate accuractely to.
 /// @return The tangent of the first argument. Free this pointer once it's done
 /// being used.
-arithmetica_function_start char *tangent(const char *number, size_t accuracy);
+arithmetica_function_start char *tangent (const char *number, size_t accuracy);
 
 /// @brief Converts a terminating decimal to a fraction reduced to its lowest
 /// terms.
@@ -148,12 +148,13 @@ arithmetica_function_start char *tangent(const char *number, size_t accuracy);
 /// @param numerator_out The output numerator of the fraction.
 /// @param denominator_out The output denominator of the fraction.
 arithmetica_function_start void
-terminating_decimal_to_fraction(const char *decimal, char *numerator_out,
-                                char *denominator_out);
+terminating_decimal_to_fraction (const char *decimal, char *numerator_out,
+                                 char *denominator_out);
 
 // Fractional number functions
 
-struct fraction {
+struct fraction
+{
   char *numerator;
   char *denominator;
 };
@@ -162,78 +163,82 @@ struct fraction {
 /// @param frac1 The first fraction to add.
 /// @param frac2 The second fraction to add.
 /// @return Where frac1 + frac2 will be stored.
-arithmetica_function_start struct fraction add_fraction(struct fraction frac1,
-                                                        struct fraction frac2);
+arithmetica_function_start struct fraction
+add_fraction (struct fraction frac1, struct fraction frac2);
 
-/// @brief Multiplies two fractions and reduces the result to its simplest form.
+/// @brief Multiplies two fractions and reduces the result to its simplest
+/// form.
 /// @param frac1 The first fraction to multiply.
 /// @param frac2 The second fraction to multiply.
 /// @return Where frac1 * frac2 will be stored.
 arithmetica_function_start struct fraction
-multiply_fraction(struct fraction frac1, struct fraction frac2);
+multiply_fraction (struct fraction frac1, struct fraction frac2);
 
-arithmetica_function_start struct fraction create_fraction(const char *n,
-                                                           const char *d);
+arithmetica_function_start struct fraction create_fraction (const char *n,
+                                                            const char *d);
 
-arithmetica_function_start void delete_fraction(struct fraction f);
+arithmetica_function_start void delete_fraction (struct fraction f);
 
 /// @brief Extracts a fraction from a string and/or converts a decimal to a
 /// fraction.
 /// @param frac The fraction as a decimal and/or in the form a/b, where a and b
 /// are decimal numbers.
 /// @return The fractional representation of `frac` in its simplest form.
-arithmetica_function_start struct fraction parse_fraction(const char *frac);
+arithmetica_function_start struct fraction parse_fraction (const char *frac);
 
-/// @brief Subtractss two fractions and reduces the result to its simplest form.
+/// @brief Subtractss two fractions and reduces the result to its simplest
+/// form.
 /// @param frac1 The first fraction to subtract.
 /// @param frac2 The second fraction to subtract.
 /// @return Where frac1 - frac2 will be stored.
 arithmetica_function_start struct fraction
-subtract_fraction(struct fraction frac1, struct fraction frac2);
+subtract_fraction (struct fraction frac1, struct fraction frac2);
 
-/// @brief Raises a fraction to a fractional power and reduces the result to its
-/// simplest form.
+/// @brief Raises a fraction to a fractional power and reduces the result to
+/// its simplest form.
 /// @param base The base of the exponentiation.
 /// @param exponent The exponent of the exponentiation.
 /// @param accuracy The accuracy of the exponentiation.
 /// @return Where frac1^frac2 will be stored.
 arithmetica_function_start struct fraction
-power_fraction(struct fraction base, struct fraction exponent, size_t accuracy);
+power_fraction (struct fraction base, struct fraction exponent,
+                size_t accuracy);
 
 // Complex number functions
 
-struct complex {
+struct complex
+{
   char *real;
   char *imaginary;
 };
 
-arithmetica_function_start struct complex create_complex_number(const char *re,
-                                                                const char *im);
-arithmetica_function_start void delete_complex_number(struct complex n);
+arithmetica_function_start struct complex
+create_complex_number (const char *re, const char *im);
+arithmetica_function_start void delete_complex_number (struct complex n);
 
 /// @brief Adds two complex numbers. If you're using this function, you know
 /// what complex numbers are.
 /// @param a The first complex number to add.
 /// @param b The second complex number to add.
 /// @return Where a + b will be stored.
-arithmetica_function_start struct complex add_complex(struct complex a,
-                                                      struct complex b);
+arithmetica_function_start struct complex add_complex (struct complex a,
+                                                       struct complex b);
 
 /// @brief Subtracts two complex numbers. If you're using this function, you
 /// know what complex numbers are.
 /// @param a The first complex number to subtract.
 /// @param b The second complex number to subtract.
 /// @return Where a - b will be stored.
-arithmetica_function_start struct complex subtract_complex(struct complex a,
-                                                           struct complex b);
+arithmetica_function_start struct complex subtract_complex (struct complex a,
+                                                            struct complex b);
 
 /// @brief Multiples two complex numbers. If you're using this function, you
 /// know what complex numbers are.
 /// @param a The first complex number to multiply.
 /// @param b The second complex number to multiply.
 /// @return Where a * b will be stored.
-arithmetica_function_start struct complex multiply_complex(struct complex a,
-                                                           struct complex b);
+arithmetica_function_start struct complex multiply_complex (struct complex a,
+                                                            struct complex b);
 
 /// @brief Divides two complex numbers. If you're using this function, you
 /// know what complex numbers are.
@@ -242,21 +247,21 @@ arithmetica_function_start struct complex multiply_complex(struct complex a,
 /// @param accuracy The number of decimals the division will be accurate to.
 /// @return Where a / b will be stored.
 arithmetica_function_start struct complex
-divide_complex(struct complex a, struct complex b, size_t accuracy);
+divide_complex (struct complex a, struct complex b, size_t accuracy);
 
 /// @brief Computes the square root of a complex number. If you're using this
 /// function, you know what complex numbers are.
 /// @param n The complex number to find the square root of.
 /// @param accuracy The number of decimals the square root will be accurate to.
 /// @return Where sqrt(n) will be stored.
-arithmetica_function_start struct complex square_root_complex(struct complex n,
-                                                              size_t accuracy);
+arithmetica_function_start struct complex
+square_root_complex (struct complex n, size_t accuracy);
 
 /// @brief Computes e^(a + bi), where a + bi is a complex number to any decimal
 /// place.
 /// @param n The complex number to exponentiate.
 /// @param accuracy The number of decimals the calculation will be accurate to.
 /// @return Where e^(a + bi) will be stored.
-arithmetica_function_start struct complex exponential_complex(struct complex n,
-                                                              size_t accuracy);
+arithmetica_function_start struct complex
+exponential_complex (struct complex n, size_t accuracy);
 #endif
---
+++ src/library/arithmetica.hpp
@@ -0,0 +1,311 @@
+#pragma once
+
+#include "arithmetica.h"
+#include <cstdlib>
+#include <cstring>
+#include <string>
+
+/// @brief Wrapper for C++
+namespace arithmetica
+{
+/// @brief Computes the inverse cosine of a number less than 1.
+/// @param number The number to compute the inverse cosine of.
+/// @param accuracy The number of decimal places to calculate accurately to.
+/// @return The inverse cosine of the first argument.
+std::string
+arccos (std::string n, size_t accuracy)
+{
+  char *answer = ::arccos (n.c_str (), accuracy);
+  std::string _answer = answer;
+  free (answer);
+  return _answer;
+}
+
+/// @brief Computes the inverse sine of a number less than 1.
+/// @param number The number to compute the inverse sine of.
+/// @param accuracy The number of decimal places to calculate accurately to.
+/// @return The inverse sine of the first argument.
+std::string
+arcsin (std::string n, size_t accuracy)
+{
+  char *answer = ::arcsin (n.c_str (), accuracy);
+  std::string _answer = answer;
+  free (answer);
+  return _answer;
+}
+
+/// @brief Computes the inverse tangent of a number less than 1.
+/// @param x The number to compute the inverse tangent of.
+/// @param accuracy The number of decimal places to calculate accurately to.
+/// @return The inverse tangent of the first argument.
+std::string
+arctan (std::string n, size_t accuracy)
+{
+  char *answer = ::arctan (n.c_str (), accuracy);
+  std::string _answer = answer;
+  free (answer);
+  return _answer;
+}
+
+/// @brief Computes the cosine of the first argument.
+/// @param number The angle to compute the cosine of, in radians.
+/// @param accuracy The number of decimal places to calculate accuractely to.
+/// @return The cosine of the first argument.
+std::string
+cosine (std::string n, size_t accuracy)
+{
+  char *answer = ::cosine (n.c_str (), accuracy);
+  std::string _answer = answer;
+  free (answer);
+  return _answer;
+}
+
+/// @brief Computes e^x, where e is Euler's number: the base to the natural
+/// logarithm.
+/// @param x The number to raise e to.
+/// @param accuracy The number of decimal places to calculate accurately to.
+/// @return The value of e^x.
+std::string
+exponential (std::string x, size_t accuracy)
+{
+  char *answer = ::exponential (x.c_str (), accuracy);
+  std::string _answer = answer;
+  free (answer);
+  return _answer;
+}
+
+/// @brief Finds the factorial of the first argument.
+/// @param n The number to find the factorial of.
+/// @return The factorial of the first argument.
+std::string
+factorial (unsigned long n)
+{
+  char *answer
+      = (char *)calloc (n * __builtin_floorf (__builtin_log10 (n) + 1) + 2, 1);
+  ::factorial (n, answer);
+  std::string _answer = answer;
+  free (answer);
+  return _answer;
+}
+
+/// @brief Converts a non-negative rational fraction to a continued fraction.
+/// @param numerator The numerator of the fraction.
+/// @param denominator The denominator of the fraction.
+/// @return An std::string containing the continued fraction
+/// representation of the input fraction. Safe to print.
+std::string
+fraction_to_continued_fraction (std::string numerator, std::string denominator)
+{
+  char *answer = ::fraction_to_continued_fraction (numerator.c_str (),
+                                                   denominator.c_str ());
+  std::string _answer = answer;
+  free (answer);
+  return _answer;
+}
+
+/// @brief Finds the greatest common divisor of the first and second integral
+/// arguments.
+/// @param a The first non-negative number to find the gcd of.
+/// @param b The second non-negative number to find the gcd of.
+/// @return The gcd of the first two arguments.
+std::string
+igcd (std::string a, std::string b)
+{
+  char *gcd = (char *)calloc (a.length () + b.length () + 3, 1);
+  ::igcd (a.c_str (), b.c_str (), gcd);
+  std::string answer = gcd;
+  free (gcd);
+  return answer;
+}
+
+/// @brief Finds the lowest common multiple of the first and second integral
+/// arguments.
+/// @param a The first non-negative number to find the lcm of.
+/// @param b The second non-negative number to find the lcm of.
+/// @return The lcm of the first two arguments.
+std::string
+ilcm (std::string a, std::string b)
+{
+  char *lcm = (char *)calloc (2 * (a.length () + b.length () + 3) + 3, 1);
+  ::ilcm (a.c_str (), b.c_str (), lcm);
+  std::string answer = lcm;
+  free (lcm);
+  return answer;
+}
+
+/// @brief Computes the natural logarithm of a non-negative number
+/// to any decimal place.
+/// @param n The number to compute the natural logarithm of.
+/// @param accuracy The number of decimal places to calculate accurately to.
+/// @return The natural logarithm of the input number.
+std::string
+natural_logarithm (std::string n, size_t accuracy)
+{
+  char *answer = ::natural_logarithm (n.c_str (), accuracy);
+  std::string _answer = answer;
+  free (answer);
+  return _answer;
+}
+
+/// @brief Computes x^n where x and n are two real numbers.
+/// @param x The real number base.
+/// @param n The real number exponent.
+/// @param accuracy The number of decimal places to calculate accurately to.
+/// @return An std::string containing x^n.
+std::string
+power (std::string x, std::string n, size_t accuracy)
+{
+  char *pow = ::power (x.c_str (), n.c_str (), accuracy);
+  std::string answer = pow;
+  free (pow);
+  return answer;
+}
+
+/// @brief Converts a repeating decimal to a fraction
+/// @param non_repeating_part The part of the fraction (including the part
+/// before the decimal place) that does not repeat. For example, in
+/// 0.3343434..., "0.3" would be the non-repeating part.
+/// @param repeating_part The part of the fraction which repeats. For example
+/// in 0.3343434..., "34" would be the repeating part.
+/// @return The output fraction.
+std::pair<std::string, std::string>
+repeating_decimal_to_fraction (const std::string &non_repeating_part,
+                               const std::string &repeating_part)
+{
+  char *numerator = (char *)calloc (
+      non_repeating_part.length () + repeating_part.length () + 1, 1);
+  char *denominator = (char *)calloc (
+      non_repeating_part.length () + repeating_part.length () + 1, 1);
+  ::repeating_decimal_to_fraction (non_repeating_part.c_str (),
+                                   repeating_part.c_str (), numerator,
+                                   denominator);
+  std::pair<std::string, std::string> answer = { numerator, denominator };
+  free (numerator);
+  free (denominator);
+  return answer;
+}
+
+/// @brief Simplies an arithmetic expression involving the five basic math
+/// operations: addition, subtraction, multiplication, division, and
+/// exponentiation.
+/// @param expression The expression to simplify, in standard computer
+/// mathematical notation.
+/// @param outputType This parameter controls the output type. Use zero to
+/// output a decimal, one to output a fraction, and two to output a mixed
+/// fraction. Output types one and two will calculate everything with maximum
+/// precision.
+/// @param accuracy The accuracy of the divisions and exponentiations
+/// performed. You only need to worry about this if you have fractional
+/// exponents in your input, or if you have divisions in output type zero.
+/// @return The simplified expression.
+std::string
+simplify_arithmetic_expression (const std::string &expression_in,
+                                int outputType, size_t accuracy)
+{
+  char *answer = ::simplify_arithmetic_expression (expression_in.c_str (),
+                                                   outputType, accuracy);
+  std::string _answer = answer;
+  free (answer);
+  return _answer;
+}
+
+/// @brief Computes the sine of the first argument.
+/// @param number The angle to compute the sine of, in radians.
+/// @param accuracy The number of decimal places to calculate accuractely to.
+/// @return The sine of the first argument.
+std::string
+sine (const std::string &number, size_t accuracy)
+{
+  char *answer = ::sine (number.c_str (), accuracy);
+  std::string _answer = answer;
+  free (answer);
+  return _answer;
+}
+
+/// @brief Computes the square root of the first argument.
+/// @param number The non-negative number to compute the square root of.
+/// @param accuracy The number of decimal places to calculate accurately to.
+/// @return The square root of the first argument.
+std::string
+square_root (const std::string &number_in, size_t accuracy)
+{
+  char *answer = ::square_root (number_in.c_str (), accuracy);
+  std::string _answer = answer;
+  free (answer);
+  return _answer;
+}
+
+/// @brief Computes the trignometric tangent of the first argument.
+/// @param number The angle to compute the tangent of, in radians.
+/// @param accuracy The number of decimal places to calculate accuractely to.
+/// @return The tangent of the first argument.
+std::string
+tangent (const std::string &number, size_t accuracy)
+{
+  char *answer = ::tangent (number.c_str (), accuracy);
+  std::string _answer = answer;
+  free (answer);
+  return _answer;
+}
+
+/// @brief Converts a terminating decimal to a fraction reduced to its lowest
+/// terms.
+/// @param decimal The decimal number to convert to a fraction.
+/// @return The output fraction.
+std::pair<std::string, std::string>
+terminating_decimal_to_fraction (const std::string &decimal)
+{
+  char *numerator = (char *)calloc (2 * decimal.length () + 1, 1);
+  char *denominator = (char *)calloc (2 * decimal.length () + 1, 1);
+  ::terminating_decimal_to_fraction (decimal.c_str (), numerator, denominator);
+  std::pair<std::string, std::string> answer = { numerator, denominator };
+  free (numerator);
+  free (denominator);
+  return answer;
+}
+
+// TODO: Port the 'fraction' struct similar to 'complex'.
+
+class Complex
+{
+public:
+  std::string real, imaginary;
+
+  Complex ();
+  Complex (std::string real, std::string imaginary)
+  {
+    this->real = real;
+    this->imaginary = imaginary;
+  }
+
+  /// @brief Adds two complex numbers. If you're using this function, you know
+  /// what complex numbers are.
+  /// @param n Adds n to the left argument.
+  /// @return Where the addition will be stored.
+  Complex
+  operator+ (Complex n)
+  {
+    struct complex i_1;
+    struct complex i_2;
+
+    i_1.real = (char *)calloc (real.length () + 1, 1);
+    i_1.imaginary = (char *)calloc (imaginary.length () + 1, 1);
+    i_2.real = (char *)calloc (n.real.length () + 1, 1);
+    i_2.imaginary = (char *)calloc (n.imaginary.length () + 1, 1);
+
+    strcpy (i_1.real, real.c_str ());
+    strcpy (i_1.imaginary, imaginary.c_str ());
+    strcpy (i_2.real, n.real.c_str ());
+    strcpy (i_2.imaginary, n.imaginary.c_str ());
+
+    struct complex result = add_complex (i_1, i_2);
+    delete_complex_number (i_1);
+    delete_complex_number (i_2);
+
+    Complex answer (result.real, result.imaginary);
+    delete_complex_number (result);
+
+    return answer;
+  }
+};
+}; // namespace arithmetica
