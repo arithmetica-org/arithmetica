1244b75fc0c835a1b21c5b2b8a860d06bbc44f0f
Avighna <74564976+avighnac@users.noreply.github.com>
2023-02-25 15:01:39 +0000

    Fixed some more valgrind memory errors.

--- src/library/igcd.c
+++ src/library/igcd.c
@@ -1,19 +1,29 @@
 #include <basic_math_operations.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 void
 igcd (const char *a_in, const char *b_in, char *gcd)
 {
-  if (strlen (a_in) == 0 || strlen (b_in) == 0)
+  if (a_in[0] == '\0' || b_in[0] == '\0')
     return;
 
-  char *a = (char *)calloc (strlen (a_in) + strlen (b_in) + 3, 1);
-  char *b = (char *)calloc (strlen (a_in) + strlen (b_in) + 3, 1);
+  // gcd(n,1) = 1
+  if (strcmp (a_in, "1") == 0 || strcmp (b_in, "1") == 0)
+    {
+      strcpy (gcd, "1");
+      return;
+    }
+
+  size_t buffer_sizes = strlen (a_in) + strlen (b_in) + 3;
+
+  char *a = (char *)calloc (buffer_sizes, 1);
+  char *b = (char *)calloc (buffer_sizes, 1);
 
   // allocate memory for some temporary variables
-  char *quotient = (char *)calloc (strlen (a_in) + strlen (b_in) + 3, 1);
-  char *temp = (char *)calloc (strlen (a_in) + strlen (b_in) + 3, 1);
+  char *quotient = (char *)calloc (buffer_sizes, 1);
+  char *temp = (char *)calloc (buffer_sizes, 1);
 
   // copy over the input numbers, ignoring sign
   (*a_in == '-') ? strcpy (a, a_in + 1) : strcpy (a, a_in);
@@ -23,15 +33,12 @@ igcd (const char *a_in, const char *b_in, char *gcd)
   while (strcmp (b, "0"))
     {
       strcpy (temp, b);
-      memset (quotient, 0, strlen (quotient));
-      memset (b, 0, strlen (b));
+      memset (quotient, 0, buffer_sizes);
+      memset (b, 0, buffer_sizes);
       divide_whole_with_remainder (a, temp, quotient, b);
-      remove_zeroes (quotient);
-      remove_zeroes (b);
       strcpy (a, temp);
     }
 
-  remove_zeroes (a);
   strcpy (gcd, a);
 
   free (a);
