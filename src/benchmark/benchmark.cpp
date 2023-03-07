2845440d8366e891f278929f642e3f1492fc3e06
avighnac <avighnakc@gmail.com>
2023-03-08 02:10:25 +0530

    Made benchmark.cpp.

---
+++ src/benchmark/benchmark.cpp
@@ -0,0 +1,298 @@
+#include "get_percent_improvement.hpp"
+#include <algorithm>
+#include <call_arithmetica.hpp>
+#include <cmath>
+#include <color.hpp>
+#include <curl/curl.h>
+#include <fstream>
+#include <get_current_directory.hpp>
+#include <iostream>
+#include <vector>
+
+static std::string
+get_input_filename (std::string function, std::string currentDir)
+{
+  return currentDir + "src/tests/arithmetica_tests/" + function + "/input.txt";
+}
+
+static std::string
+get_expected_filename (std::string function, std::string currentDir)
+{
+  return currentDir + "src/tests/arithmetica_tests/" + function
+         + "/expected.txt";
+}
+
+static size_t
+write_callback (char *ptr, size_t size, size_t nmemb, void *userdata)
+{
+  // Do nothing with the received data
+  return size * nmemb;
+}
+
+std::string get_cpu_name ();
+
+// Linux
+#ifdef __linux__
+#include <cpuid.h>
+std::string
+get_cpu_name ()
+{
+  char CPUBrandString[0x40];
+  unsigned int CPUInfo[4] = { 0, 0, 0, 0 };
+
+  __cpuid (0x80000000, CPUInfo[0], CPUInfo[1], CPUInfo[2], CPUInfo[3]);
+  unsigned int nExIds = CPUInfo[0];
+
+  memset (CPUBrandString, 0, sizeof (CPUBrandString));
+
+  for (unsigned int i = 0x80000000; i <= nExIds; ++i)
+    {
+      __cpuid (i, CPUInfo[0], CPUInfo[1], CPUInfo[2], CPUInfo[3]);
+
+      if (i == 0x80000002)
+        memcpy (CPUBrandString, CPUInfo, sizeof (CPUInfo));
+      else if (i == 0x80000003)
+        memcpy (CPUBrandString + 16, CPUInfo, sizeof (CPUInfo));
+      else if (i == 0x80000004)
+        memcpy (CPUBrandString + 32, CPUInfo, sizeof (CPUInfo));
+    }
+
+  return CPUBrandString;
+}
+// Windows
+#elif _WIN32
+std::string
+get_cpu_name ()
+{
+  return "some random windows cpu idk (probs intel) use linux";
+}
+#endif
+
+static std::vector<std::string>
+split_string (std::string s, char ch)
+{
+  std::vector<std::string> answer;
+  std::string part;
+  for (auto &i : s)
+    {
+      if (i != ch)
+        part.push_back (i);
+      else
+        {
+          answer.push_back (part);
+          part.clear ();
+        }
+    }
+  if (!part.empty ())
+    answer.push_back (part);
+  return answer;
+}
+
+static std::string
+color_digits (std::string s, std::string col)
+{
+  std::string answer;
+  for (auto &i : s)
+    {
+      if (isdigit (i))
+        answer += color (std::string (1, i), col);
+      else
+        answer.push_back (i);
+    }
+  return answer;
+}
+
+int
+main (int argc, char **argv)
+{
+  std::cout << "Benchmarking arithmetica:\n\n";
+  std::vector<std::string> functions = { "arcsin",
+                                         "arccos",
+                                         "arctan",
+                                         "cosine",
+                                         "factorial",
+                                         "exponential",
+                                         "fraction_to_continued_fraction",
+                                         "igcd",
+                                         "ilcm",
+                                         "natural_logarithm",
+                                         "power",
+                                         "repeating_decimal_to_fraction",
+                                         "simplify_arithmetic_expression",
+                                         "sine",
+                                         "square_root",
+                                         "tangent",
+                                         "terminating_decimal_to_fraction",
+                                         "add_fraction",
+                                         "multiply_fraction",
+                                         "power_fraction",
+                                         "construct_regular_polygon" };
+  std::sort (functions.begin (), functions.end ());
+
+  bool benchmark = false;
+  std::vector<double> benchmark_hertz;
+
+  std::string benchmark_identifier;
+
+  if (argc > 1)
+    {
+      std::vector<std::string> _functions;
+      for (auto i = 1; i < argc; i++)
+        {
+          if (std::string (argv[i]) == "--unique-identifier")
+            {
+              if (i + 1 < argc)
+                {
+                  benchmark_identifier = argv[i + 1];
+                  i++;
+                }
+              else
+                {
+                  std::cout << "Error: --unique-identifier requires an "
+                               "identifier.\n";
+                  return 1;
+                }
+              continue;
+            }
+
+          _functions.push_back (argv[i]);
+          benchmark = true;
+        }
+      if (benchmark)
+        functions = _functions;
+    }
+
+  std::string currentDir = get_current_directory ();
+  std::replace (currentDir.begin (), currentDir.end (), '\\', '/');
+  if (currentDir.find ("build") != std::string::npos)
+    currentDir = currentDir.substr (0, currentDir.find ("build"));
+  if (currentDir.back () != '/')
+    currentDir.push_back ('/');
+
+  for (auto i = 0; i < functions.size (); i++)
+    {
+      std::cout << "<" << i + 1 << "> "
+                << color (functions[i] + ":", "Magenta") << '\n';
+
+      int currentTest = 0;
+      std::ifstream expectedFile (
+          get_expected_filename (functions[i], currentDir));
+      std::ifstream inputFile (get_input_filename (functions[i], currentDir));
+
+      if (!expectedFile || !inputFile)
+        {
+          std::cout << "Couldn't open test files!\n";
+          std::cout << currentDir << "\n";
+          continue;
+        }
+
+      std::string input;
+      std::vector<std::vector<std::string> > arithmetica_inputs;
+      while (std::getline (inputFile, input))
+        {
+          std::string inputsStr;
+          std::string spaces
+              = "   " + std::string (std::to_string (i + 1).length (), ' ');
+          currentTest++;
+          auto _inputs = split_string (input, ' ');
+          std::vector<std::string> inputs = { functions[i] };
+          std::copy (_inputs.begin (), _inputs.end (),
+                     std::back_inserter (inputs));
+          arithmetica_inputs.push_back (inputs);
+        }
+      inputFile.close ();
+      expectedFile.close ();
+
+      // Calculate how many times the function runs in 5 seconds.
+      int times = 0;
+      double time_elapsed = 0;
+      while (time_elapsed < 5000)
+        {
+          for (auto &i : arithmetica_inputs)
+            {
+              double timeMS;
+              call_arithmetica (i, timeMS);
+              time_elapsed += timeMS;
+              // Since we're using GitHub Actions, we can't keep printing to
+              // stdout, so we'll just print the last result.
+              // std::cout << "\r  " << color (std::to_string (times), "Green")
+              //           << " runs in "
+              //           << color (std::to_string (time_elapsed), "Green")
+              //           << " ms" << std::flush;
+            }
+          times++;
+        }
+
+      benchmark_hertz.push_back (times * 1000 / time_elapsed);
+      std::cout << "\r  " << color (std::to_string (times), "Green")
+                << " runs in "
+                << color (std::to_string (time_elapsed), "Green")
+                << " ms\n    "
+                << color (std::to_string (times * 1000 / time_elapsed),
+                          "Green")
+                << " Hz" << std::endl;
+    }
+
+  if (benchmark_identifier.empty ())
+    {
+      std::cout << "No unique identifier was provided. Use "
+                   "--unique-identifier to provide one, if you want to upload "
+                   "the results of the benchmark.\n";
+      return 0;
+    }
+
+  std::cout << "Uploading benchmark results!\n";
+  std::string json_content = "{\"identifier\":%20\"" + benchmark_identifier
+                             + "\",%0D%0A\"cpu_name\":\"" + get_cpu_name ()
+                             + "\",%0D%0A\"benchmarks\":{";
+  for (auto i = 0; i < functions.size (); i++)
+    {
+      json_content += "\"" + functions[i] + "\":%20"
+                      + std::to_string (benchmark_hertz[i]);
+      if (i != functions.size () - 1)
+        json_content += ",%20%0D%0A";
+    }
+  json_content += "}}";
+
+  std::string curl_postfields
+      = "entry.1369257619=" + json_content
+        + "&fvv=1&"
+          "partialResponse=%5Bnull%2Cnull%2C%22-4957582068535559811%22%5D&"
+          "pageHistory=0&fbzx=-4957582068535559811";
+
+  CURL *curl = curl_easy_init ();
+  if (curl)
+    {
+      curl_easy_setopt (curl, CURLOPT_URL,
+                        "https://docs.google.com/forms/d/e/"
+                        "1FAIpQLSc9Qdp2sxBD9sal-0YeoeIg6ys_OJ-"
+                        "yek16CsndcFRvejrt5A/formResponse");
+      curl_easy_setopt (curl, CURLOPT_POSTFIELDS, curl_postfields.c_str ());
+      curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, write_callback);
+
+      CURLcode res = curl_easy_perform (curl);
+      if (res != CURLE_OK)
+        std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror (res)
+                  << std::endl;
+
+      curl_easy_cleanup (curl);
+    }
+
+  std::cout << "Benchmark results uploaded!\n";
+
+  // If this is not running on a GH Actions runner, then we'll just exit.
+  if (benchmark_identifier.find ("commit") == std::string::npos)
+    return 0;
+
+  // We can now compare the results of the benchmark to the previous results.
+  // format: commit-<commit_number>
+  int commit_number = std::stoi (benchmark_identifier.substr (7));
+  std::string previous_commit = "commit-" + std::to_string (commit_number - 1);
+
+  std::cout << "Comparing benchmark results to previous commit:\n";
+  for (auto &i :
+       get_percent_improvement (previous_commit, benchmark_identifier))
+    {
+      std::cout << "  " << i.first << ": " << i.second << "% improvement.\n";
+    }
+}
---
+++ src/benchmark/get_percent_improvement.hpp
@@ -0,0 +1,137 @@
+#pragma once
+
+#include <algorithm>
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
+#include <vector>
+
+static void
+replace_all (std::string &str, const std::string &from, const std::string &to)
+{
+  if (from.empty ())
+    return;
+  size_t start_pos = 0;
+  while ((start_pos = str.find (from, start_pos)) != std::string::npos)
+    {
+      str.replace (start_pos, from.length (), to);
+      start_pos += to.length ();
+    }
+}
+
+static inline size_t
+curl_get_response (char *buffer, size_t itemsize, size_t nitems,
+                   void *ignorethis)
+{
+  size_t bytes = itemsize * nitems;
+  return bytes;
+}
+
+static size_t
+curl_write_data (char *ptr, size_t size, size_t nmemb, void *inputstream)
+{
+  std::ostringstream *stream = (std::ostringstream *)inputstream;
+  size_t count = size * nmemb;
+  stream->write (ptr, count);
+  return count;
+}
+
+static std::vector<std::pair<std::string, double> >
+get_uid_details (std::string uid, std::vector<nlohmann::json> json_objects)
+{
+  std::vector<std::pair<std::string, double> > uid_details;
+  for (auto &json_object : json_objects)
+    {
+      if (json_object["identifier"] != uid)
+        {
+          continue;
+        }
+
+      for (auto &[name, hertz] : json_object["benchmarks"].items ())
+        {
+          std::string function_name = name;
+          double runtime_hertz = hertz;
+          uid_details.push_back (
+              std::make_pair (function_name, runtime_hertz));
+        }
+    }
+  return uid_details;
+}
+
+// Return a vector of percentage improvements / regressions, positive values
+// are improvements, negative values are regressions.
+std::vector<std::pair<std::string, double> >
+get_percent_improvement (std::string uid_1, std::string uid_2)
+{
+  CURL *curl = curl_easy_init ();
+  std::ostringstream stream;
+
+  std::string url = "https://docs.google.com/forms/d/e/"
+                    "1FAIpQLSc9Qdp2sxBD9sal-0YeoeIg6ys_OJ-yek16CsndcFRvejrt5A/"
+                    "viewanalytics";
+
+  curl_easy_setopt (curl, CURLOPT_URL, url.c_str ());
+  curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, curl_write_data);
+  curl_easy_setopt (curl, CURLOPT_WRITEDATA, &stream);
+  curl_easy_perform (curl);
+  curl_easy_cleanup (curl);
+
+  std::string respose_contents = stream.str ();
+
+  // Each json object starts with <{\"identifier\"> and ends with <}}>, where
+  // <> represent quotes.
+  std::vector<std::string> json_objects_str;
+  std::string::size_type start = respose_contents.find ("{\\\"identifier");
+  while (start != std::string::npos)
+    {
+      std::string::size_type end = respose_contents.find ("}}", start);
+      if (end != std::string::npos)
+        {
+          json_objects_str.push_back (
+              respose_contents.substr (start, end - start + 2));
+          start = respose_contents.find ("{\\\"identifier", end);
+        }
+      else
+        {
+          start = end;
+        }
+    }
+
+  for (auto &json_object : json_objects_str)
+    {
+      replace_all (json_object, "\\n", "");
+      replace_all (json_object, "\\", "");
+    }
+
+  // Parse the json objects.
+  std::vector<nlohmann::json> json_objects;
+  for (auto &json_object_str : json_objects_str)
+    {
+      json_objects.push_back (nlohmann::json::parse (json_object_str));
+    }
+
+  // Get the details of the two uids.
+  std::vector<std::pair<std::string, double> > uid_1_details
+      = get_uid_details (uid_1, json_objects);
+  std::vector<std::pair<std::string, double> > uid_2_details
+      = get_uid_details (uid_2, json_objects);
+
+  // Calculate the percentage improvement / regression. Each element in the
+  // vector is the number of runs per second (Hz).
+  std::vector<std::pair<std::string, double> > percent_improvements;
+  for (auto &uid_1_detail : uid_1_details)
+    {
+      for (auto &uid_2_detail : uid_2_details)
+        {
+          if (uid_1_detail.first == uid_2_detail.first)
+            {
+              double percent_improvement
+                  = (uid_1_detail.second - uid_2_detail.second)
+                    / uid_1_detail.second;
+              percent_improvements.push_back (
+                  std::make_pair (uid_1_detail.first, percent_improvement));
+            }
+        }
+    }
+
+  return percent_improvements;
+}
