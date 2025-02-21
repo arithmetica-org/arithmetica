4c834c43af716c3d5451bcd727adcafc20866975
Avighna <74564976+avighnac@users.noreply.github.com>
2025-02-21 11:16:26 +0000

    Added edge case for algexpr::algexpr

--- src/library/algparser/algexpr.cpp
+++ src/library/algparser/algexpr.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <array>
 
+#include <iostream>
 namespace arithmetica {
 std::vector<std::string> algexpr::get_funcs() const {
   std::vector<std::string> funcs = {
@@ -147,7 +148,8 @@ int algexpr::bound(const std::string &s, long long i, int incr) {
   if (incr == -1 and is_closing_bracket(s[i - 1])) {
     std::size_t open = opening_bracket(s, i - 1);
     for (auto &func : get_funcs()) {
-      if (s.rfind(func, open - 1) != std::string::npos) {
+      auto pos = s.rfind(func, open - 1);
+      if (pos != std::string::npos and pos + func.length() == open) {
         return open - func.length();
       }
     }
@@ -226,11 +228,13 @@ algexpr::algexpr(std::string s) : l(nullptr), r(nullptr) {
       s = s.substr(l, r - l + 1);
     }
   }
+  std::cout << "expr: " << s << '\n';
   // add multiplication signs before and after ^ and /
   for (auto &c : std::string("^/")) {
     std::size_t i = -1;
     while ((i = s.find(c, i + 1)) != std::string::npos) {
       auto br = bound(s, i, 1), bl = bound(s, i, -1);
+      std::cout << "c: " << c << ", bl: " << bl << ", br: " << br << '\n';
       if (br != (long long)s.length() - 1 and !is_sign(s[br + 1]) and
           !is_bracket(s[br + 1])) {
         s.insert(br + 1, "*"); // add ahead
